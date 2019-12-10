1b55c51d6efefef54c3f1526017d8d1cd3292ae4
davethom1 <39127960+davethom1@users.noreply.github.com>
2019-12-10 16:39:51 -0500

    David Thomas

--- nacjd_converter.py
+++ nacjd_converter.py
@@ -27,3 +27,34 @@ property_file = proc_data_folder + "property.csv"
 # make the victim file
 # input to victim file: agencies, NIBRS_VICTIM, NIBRS_incident, NIBRS_OFFENSE_TYPE, NIBRS_VICTIM_CIRCUMSTANCES
 
+victim_file = proc_data_folder + "victim.csv"
+
+# make the offender file
+# input to offender file: agencies, nibrs_offender, nibrs_incident
+
+offender_file = proc_data_folder + "offender.csv"
+
+# make the arrestee file
+# input to arrestee: agencies, NIBRS_ARRESTEE, NIBRS_incident, NIBRS_ARRESTEE_WEAPON
+
+arrestee_file = proc_data_folder + "arrestee.csv"
+
+# make the group B file
+# input to group B: agencies, nibrs_incident, NIBRS_ARRESTEE, NIBRS_OFFENSE, NIBRS_WEAPON
+
+groupb_file = proc_data_folder + "groupb.csv"
+
+# make the cleared file
+#input to cleared: agencies, NIBRS_incident, NIBRS_OFFENSE
+
+cleared_file = proc_data_folder + "cleared.csv"
+
+# make the recover file
+# input to recover: agencies, NIBRS_incident, NIBRS_PROPERTY, NIBRS_PROP_DESC, NIBBRS_SUSPECTED_DRUG, NIBRS_OFFENSE
+
+recover_file = proc_data_folder + "recover.csv"
+
+# make the win_arrest file
+# input to win_arrest: agencies, NIBRS_incident, NIBRS_ARRESTEE, NIBRS_ARRESTEE_WEAPON
+
+win_arrest_file = proc_data_folder + "win_arrest.csv"
