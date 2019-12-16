9285a5188484a17e0bcaac34b7f3d0d582e585b8
davethom1 <39127960+davethom1@users.noreply.github.com>
2019-12-16 10:15:22 -0500

    Creates agency, incident, and admin dataframes

--- nacjd_converter.py
+++ nacjd_converter.py
@@ -19,11 +19,38 @@ proc_data_folder = "data/processed/"
 admin_file = proc_data_folder + "admin.csv"
 # create dataframe
 df1 = pandas.read_csv(fbi_data_folder + "agencies.csv", index_col = 'YEARLY_AGENCY_ID')
+
 # grab state_id and ori
 df.get (["STATE_ID", "ORI"])
+
+# grab state_id and ori along with agency_id by index number
+df1admin = df1.iloc[:, [14, 2]]
+
+
+#print (df1admin)
+
+#check type of newly created dataframe
+print(type(df1admin))
+
+
 df2 = pandas.read_csv(fbi_data_folder + "NIBRS_incident.csv", index_col = 'DATA_YEAR')
 
+# select "AGENCY_ID", "INCIDENT_ID", "INCIDENT_DATE", "REPORT_DATE_FLAG", "INCIDENT_HOUR", "CLEARED_EXCEPT_ID", "CLEARED_EXCEPT_DATE", "CARGO_THEFT_FLAG"
+df2admin = df2.iloc[:, [1, 5, 6, 7, 8, 9, 10, 3]]
 
+# print (df2admin)
+
+# check type of newly created dataframe
+print(type(df2admin))
+
+# sort dataframe by AGENCY_ID
+df2admin.sort_values(["AGENCY_ID"], ascending=True)
+
+# concatenate df1admin with df2admin
+# Key variable is AGENCY_ID
+
+dfadmin = pandas.merge(df1admin, df2admin, how='inner', on=['AGENCY_ID'])
+print(dfadmin)
 # make the offense file
 # inputs to offense file: agencies, NIBRS_OFFENSE, NIBRS_SUSPECT_USING, NIBRS_CRIMINAL_ACT, NIBRS_WEAPON, NIBRS_BIAS_MOTIVATION
 
